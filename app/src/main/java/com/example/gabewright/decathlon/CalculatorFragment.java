1f00f5c92c23ed233a4318063cf38513395222ef
gabeswright <gwright@cord.edu>
2015-04-29 16:41:38 -0500

    I don't think Ive made any changes since last time, but just making sure

--- app/src/main/java/com/example/gabewright/decathlon/CalculatorFragment.java
+++ app/src/main/java/com/example/gabewright/decathlon/CalculatorFragment.java
@@ -38,68 +38,77 @@ public class CalculatorFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_calculator, container, false);
+    }
 
-        one = (EditText) getView().findViewById(R.id.et100);
-        sp = (EditText) getView().findViewById(R.id.etsp);
-        lj = (EditText) getView().findViewById(R.id.etlj);
-        hj = (EditText) getView().findViewById(R.id.ethj);
-        four = (EditText) getView().findViewById(R.id.et400);
-        hurdles = (EditText) getView().findViewById(R.id.et110h);
-        discus = (EditText) getView().findViewById(R.id.etdiscus);
-        pv = (EditText) getView().findViewById(R.id.etpv);
-        jav = (EditText) getView().findViewById(R.id.etjav);
-        fifteen = (EditText) getView().findViewById(R.id.et1500);
-        submit = (TextView) getView().findViewById(R.id.tvsubmit);
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        mapViews(view);
 
         submit.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CalculatorFragment.this, activity3.class);
-                double onetime = Double.parseDouble(one.getText().toString());
-                double spd = Double.parseDouble(sp.getText().toString());
-                double ljd = Double.parseDouble(lj.getText().toString());
-                double hjh = Double.parseDouble(hj.getText().toString());
-                double fourtime = Double.parseDouble(four.getText().toString());
-                double hurdletime = Double.parseDouble(hurdles.getText().toString());
-                double discd = Double.parseDouble(discus.getText().toString());
-                double pvh = Double.parseDouble(pv.getText().toString());
-                double javd = Double.parseDouble(jav.getText().toString());
-                double fifteentime = Double.parseDouble(fifteen.getText().toString());
-
-                int onescore;
-                int fourscore;
-                int fifteenscore;
-                int hurdlescore;
-                int spscore;
-                int ljscore;
-                int hjscore;
-                int pvscore;
-                int discusscore;
-                int javscore;
-                int totalscore;
-
-                onescore = (int) Math.pow((25.437*(18-onetime)),1.81);
-                fourscore = (int) Math.pow((1.53775*(82-fourtime)),1.81);
-                fifteenscore = (int) Math.pow((.03768*(480-fifteentime)),1.85);
-                hurdlescore = (int) Math.pow((5.74352*(28.5-hurdletime)),1.92);
-                spscore = (int) Math.pow((51.39*(spd-1.5)),1.05);
-                ljscore = (int) Math.pow((.14354*(ljd-2.2)),1.4);
-                hjscore = (int) Math.pow((.8465*(hjh-.75)),1.42);
-                pvscore = (int) Math.pow((.2797*(pvh-1)),1.35);
-                discusscore = (int) Math.pow((12.91*(discd-4)),1.1);
-                javscore = (int) Math.pow((10.14*(javd-7)),1.08);
-
-                totalscore = onescore + fourscore + fifteenscore +hurdlescore + spscore + ljscore + hjscore + pvscore + discusscore + javscore;
-
-                intent.putExtra("score", totalscore);
-                startActivity(intent);
-
-
-
+                calculateScores();
             }
         });
+    }
+
+    private void calculateScores() {
+        Intent intent = new Intent(getActivity(), CalculatorResult.class);
+        double onetime = Double.parseDouble(one.getText().toString());
+        double spd = Double.parseDouble(sp.getText().toString());
+        double ljd = Double.parseDouble(lj.getText().toString());
+        double hjh = Double.parseDouble(hj.getText().toString());
+        double fourtime = Double.parseDouble(four.getText().toString());
+        double hurdletime = Double.parseDouble(hurdles.getText().toString());
+        double discd = Double.parseDouble(discus.getText().toString());
+        double pvh = Double.parseDouble(pv.getText().toString());
+        double javd = Double.parseDouble(jav.getText().toString());
+        double fifteentime = Double.parseDouble(fifteen.getText().toString());
+
+        int onescore;
+        int fourscore;
+        int fifteenscore;
+        int hurdlescore;
+        int spscore;
+        int ljscore;
+        int hjscore;
+        int pvscore;
+        int discusscore;
+        int javscore;
+        int totalscore;
+
+        onescore = (int) Math.pow((25.437*(18-onetime)),1.81);
+        fourscore = (int) Math.pow((1.53775*(82-fourtime)),1.81);
+        fifteenscore = (int) Math.pow((.03768*(480-fifteentime)),1.85);
+        hurdlescore = (int) Math.pow((5.74352*(28.5-hurdletime)),1.92);
+        spscore = (int) Math.pow((51.39*(spd-1.5)),1.05);
+        ljscore = (int) Math.pow((.14354*(ljd-2.2)),1.4);
+        hjscore = (int) Math.pow((.8465*(hjh-.75)),1.42);
+        pvscore = (int) Math.pow((.2797*(pvh-1)),1.35);
+        discusscore = (int) Math.pow((12.91*(discd-4)),1.1);
+        javscore = (int) Math.pow((10.14*(javd-7)),1.08);
+
+        totalscore = onescore + fourscore + fifteenscore +hurdlescore + spscore + ljscore + hjscore + pvscore + discusscore + javscore;
+
+        intent.putExtra("score", totalscore);
+        startActivity(intent);
+
+
 
     }
 
-
+    private void mapViews(View view) {
+        one = (EditText) view.findViewById(R.id.et100);
+        sp = (EditText) view.findViewById(R.id.etsp);
+        lj = (EditText) view.findViewById(R.id.etlj);
+        hj = (EditText) view.findViewById(R.id.ethj);
+        four = (EditText) view.findViewById(R.id.et400);
+        hurdles = (EditText) view.findViewById(R.id.et110h);
+        discus = (EditText) view.findViewById(R.id.etdiscus);
+        pv = (EditText) view.findViewById(R.id.etpv);
+        jav = (EditText) view.findViewById(R.id.etjav);
+        fifteen = (EditText) view.findViewById(R.id.et1500);
+        submit = (TextView) view.findViewById(R.id.tvsubmit);
+    }
 }
